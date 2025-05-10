a972848b4c2c816a7cd7dd448cf3b923544e740a
Max Averin <amax0703@gmail.com>
2025-05-10 21:56:49 +0200

    Delete docs

--- api/main.py
+++ api/main.py
@@ -1,21 +1,28 @@
-from fastapi import Depends, FastAPI, Request, Response
-from fastapi.middleware.cors import CORSMiddleware
-from prometheus_client import make_asgi_app, generate_latest, multiprocess, CollectorRegistry
-from sqlmodel import SQLModel, Session
-from starlette.responses import JSONResponse
-from starlette import status
-
 from db.config import settings
 from db.errors import (
-    PhoneCodeError, PhoneError,
-    TimezoneError, WrongDatetimeError,
+    PhoneCodeError,
+    PhoneError,
+    TimezoneError,
+    WrongDatetimeError,
 )
 from db.sample_data import add_sample_data
 from db.sessions import engine
+from fastapi import Depends, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from prometheus_client import CollectorRegistry, make_asgi_app, multiprocess
 from routers import (
-    users, phone_codes, timezones, tags,
-    customers, mailouts, messages, web,
+    customers,
+    mailouts,
+    messages,
+    phone_codes,
+    tags,
+    timezones,
+    users,
+    web,
 )
+from sqlmodel import SQLModel
+from starlette import status
+from starlette.responses import JSONResponse
 from utils.logging import log_request_info
 
 app = FastAPI(
@@ -23,20 +30,20 @@ app = FastAPI(
     version=settings.version,
     description=settings.description,
     openapi_prefix=settings.openapi_prefix,
-    docs_url=settings.docs_url,
-    openapi_url=settings.openapi_url,
+    docs_url=None,
+    openapi_url=None,
 )
 
-app.include_router(users.router, tags=['Authentication'])
+app.include_router(users.router, tags=["Authentication"])
 
 routers = (
-    (phone_codes.router, 'Phone Codes'),
-    (timezones.router, 'Timezones'),
-    (tags.router, 'Tags'),
-    (customers.router, 'Customers'),
-    (mailouts.router, 'Mailouts'),
-    (messages.router, 'Messages'),
-    (web.router, 'Web'),
+    (phone_codes.router, "Phone Codes"),
+    (timezones.router, "Timezones"),
+    (tags.router, "Tags"),
+    (customers.router, "Customers"),
+    (mailouts.router, "Mailouts"),
+    (messages.router, "Messages"),
+    (web.router, "Web"),
 )
 
 for router, tags in routers:
@@ -48,20 +55,20 @@ for router, tags in routers:
     )
 
 origins = [
-    'http://localhost:8000',
-    'http://localhost:8080',
+    "http://localhost:8000",
+    "http://localhost:8080",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
-@app.on_event('startup')
+@app.on_event("startup")
 async def init_tables():
     # Disable function if Celery launched
     SQLModel.metadata.drop_all(engine)
@@ -73,7 +80,7 @@ async def init_tables():
 async def phone_code_exception_handler(request: Request, exc: PhoneCodeError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={'message': 'Phone code is not a string of 3 digits'},
+        content={"message": "Phone code is not a string of 3 digits"},
     )
 
 
@@ -81,7 +88,7 @@ async def phone_code_exception_handler(request: Request, exc: PhoneCodeError):
 async def phone_exception_handler(request: Request, exc: PhoneError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={'message': 'Phone is not a string of 7 digits'},
+        content={"message": "Phone is not a string of 7 digits"},
     )
 
 
@@ -89,7 +96,7 @@ async def phone_exception_handler(request: Request, exc: PhoneError):
 async def timezone_exception_handler(request: Request, exc: TimezoneError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={'message': 'Timezone is not in the list of timezones'},
+        content={"message": "Timezone is not in the list of timezones"},
     )
 
 
@@ -97,13 +104,13 @@ async def timezone_exception_handler(request: Request, exc: TimezoneError):
 async def datetime_exception_handler(request: Request, exc: WrongDatetimeError):
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content={'message': 'Finish time before start time'},
+        content={"message": "Finish time before start time"},
     )
 
 
-@app.get('/')
+@app.get("/")
 async def root():
-    return {'Say': 'Hello!'}
+    return {"Say": "Hello!"}
 
 
 def make_metrics_app():
@@ -113,8 +120,8 @@ def make_metrics_app():
 
 
 metrics_app = make_metrics_app()
-app.mount('/metrics', metrics_app)
+app.mount("/metrics", metrics_app)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_tables()
